e7c30a8cea1e7b53eb745eee26d8d6b80015e89a
Ali Mohammadiasl <147754644+amohammadiasl@users.noreply.github.com>
2023-10-13 10:38:18 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -19,11 +19,11 @@ public class Card {
 
    /**
 	 * Creates a new <code>Card</code> instance.
-	 * @param cardRank  a <code>String</code> value
+	 * The parameter cardRank  a <code>String</code> value
 	 *                  containing the rank of the card
-	 * @param cardSuit  a <code>String</code> value
+	 * The parameter cardSuit  a <code>String</code> value
 	 *                  containing the suit of the card
-	 * @param cardPointValue an <code>int</code> value
+	 * The parameter cardPointValue an <code>int</code> value
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
@@ -36,7 +36,7 @@ public class Card {
 
 	/**
 	 * Accesses this <code>Card's</code> suit.
-	 * @return this <code>Card's</code> suit.
+	 * return this <code>Card's</code> suit.
 	 */
 	public String suit() {
 		return suit;
@@ -44,7 +44,7 @@ public class Card {
 
 	/**
 	 * Accesses this <code>Card's</code> rank.
-	 * @return this <code>Card's</code> rank.
+	 * return this <code>Card's</code> rank.
 	 */
 	public String rank() {
 		return rank;
@@ -52,17 +52,16 @@ public class Card {
 
    /**
 	 * Accesses this <code>Card's</code> point value.
-	 * @return this <code>Card's</code> point value.
+	 * return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
 		return pointValue;
 	}
 
 	/** Compare this card with the argument.
-	 * @param otherCard the other card to compare to this
-	 * @return true if the rank, suit, and point value of this card
-	 *              are equal to those of the argument;
-	 *         false otherwise.
+	 * The parameter otherCard the other card to compare to this
+	 * return true if the rank, suit, and point value of this card
+	 *              are equal to those of the argument
 	 */
 	public boolean matches(Card otherCard) {
 		return otherCard.suit().equals(this.suit())
@@ -70,16 +69,7 @@ public class Card {
 			&& otherCard.pointValue() == this.pointValue();
 	}
 
-	/**
-	 * Converts the rank, suit, and point value into a string in the format
-	 *     "[Rank] of [Suit] (point value = [PointValue])".
-	 * This provides a useful way of printing the contents
-	 * of a <code>Deck</code> in an easily readable format or performing
-	 * other similar functions.
-	 *
-	 * @return a <code>String</code> containing the rank, suit,
-	 *         and point value of the card.
-	 */
+	
 	@Override
 	public String toString() {
 		return rank + " of " + suit + " (point value = " + pointValue + ")";
